f2126b58581142784864aca01db974051d82bc4f
Anurag <anuragmarketing101@gmail.com>
2022-12-25 02:41:19 +0530

    videoPlaceholder-updated

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,11 +1,15 @@
 import React from 'react'
-import {Stack, Box} from "@mui/material"
+import {Stack, Box, CircularProgress} from "@mui/material"
 import VideoCard from './VideoCard'
 import ChannelCard from './ChannelCard'
 
 function Videos({videos , direction}) {
     
-    
+  if(!videos) return <Box sx={{backgroundColor: "black", zIndex: "100" , position: "fixed", top: "0", right: "0", left:"0", bottom: "0", width: "100vw", height: "100vh" , display: 'flex', justifyContent: "center", alignItems: "center" }}> 
+    <CircularProgress /> 
+    </Box>
+   
+
   return (
     <Stack direction={direction || "row"} sx={{ alignItems: "center", justifyContent: "center"}}  flexWrap="wrap"
     justifyContent="start" gap={2}
